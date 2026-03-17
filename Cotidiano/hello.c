923082b0171f3004ba0fb377f09d51a226524367
dayis.txtt <dayana.gomezrodriguez@ucr.ac.cr>
2026-03-17 09:45:11 -0600

    hello con thread

--- Cotidiano/hello.c
+++ Cotidiano/hello.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <pthread.h>
 
-int main () {
+void* imprimirthread(void* arg);
+
+int main () {	
+	pthread_t threadID;
+	int id = 1;
+
+	pthread_create(&threadID, NULL, imprimirthread, &id);
+	pthread_join(threadID, NULL);
+
+	return 0;
+}
+
+void* imprimirthread(void* arg){
+	int id = *(int*)arg;
+
 	printf("Hello World\n");
+	printf("Soy el hilo %d\n", id);
+
+	return NULL;
 }
