5a19c08bb57066771b79289a9c642ad2211aa476
Maikel <Maikel.Acosta@ucr.ac.cr>
2026-03-20 07:53:09 -0600

    Ejercicio de helloworld completo con pthreads

--- Cotidiano/hello.c
+++ Cotidiano/hello.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <pthread.h>
 
+
 void* imprimirthread(void* arg);
 
 int main () {	
-	pthread_t threadID;
-	int id = 1;
-
-	pthread_create(&threadID, NULL, imprimirthread, &id);
-	pthread_join(threadID, NULL);
-
+	pthread_t threadID[4];
+	int status;
+	int i;
+	for(i = 0;i < 4; i++){
+		status = pthread_create(&threadID[i], NULL, imprimirthread, &i);
+	}
+	
+	for(i=0;i<4;i++){
+		pthread_join(threadID[i], NULL);
+	}
 	return 0;
 }
 
 void* imprimirthread(void* arg){
 	int id = *(int*)arg;
 
-	printf("Hello World\n");
-	printf("Soy el hilo %d\n", id);
+	printf("Hello World\tSoy el hilo %d\n", id);
 
 	return NULL;
 }
