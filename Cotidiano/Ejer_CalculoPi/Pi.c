464db08971d9de02017d56dbe519ce8caf960519
Maikel <Maikel.Acosta@ucr.ac.cr>
2026-04-17 08:09:51 -0600

    Creacion de pi

---
+++ Cotidiano/Ejer_CalculoPi/Pi.c
@@ -0,0 +1,19 @@
+#include <omp.h>
+#include <stdio.h>
+
+static long num_steps = 100000;
+double step;
+
+int main(){
+	int i;
+	double x, pi, sum = 0.0;
+	step = 1.0/(double) num_steps;
+	double tdata = omp_get_wtime();
+	for(i=0; i<num_steps; i++){
+		x = (i+0.5)*step;
+		sum = sum + 4.0/(1.0+x*x);
+	}
+	pi = step*sum;
+	tdata = omp_get_wtime()-tdata;
+	printf("pi = %f in %f secs\n", pi, tdata);
+}
